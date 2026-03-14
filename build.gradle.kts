2d1bc0536cd17ba8a7bc893126b1f837039a65fd
Onur Civanoğlu <onur.civanoglu@outlook.com>
2026-03-15 00:34:22 +0300

    Align Kotlin with Cloudstream pre-release

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,8 @@ buildscript {
         classpath("com.android.tools.build:gradle:8.7.3")
         // Cloudstream gradle plugin which makes everything work and builds plugins
         classpath("com.github.recloudstream:gradle:-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
+        // Keep this aligned with the Cloudstream pre-release stubs.
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
     }
 }
 
